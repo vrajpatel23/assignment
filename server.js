68c8db41486d3c05174fdd4824f5564ebe248e5c
vrajpatel23 <113646894+vrajpatel23@users.noreply.github.com>
2022-12-04 10:55:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,24 +10,32 @@ const passportJWT = require('passport-jwt');
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
+let vrajjwtextract = passportJWT.ExtractJwt;
+let vrajplan = passportJWT.Strategy;
 
-// Configure its options
-let jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
 
-jwtOptions.secretOrKey = process.env.JWT_SECRET;
+let vrahoptionjwt = {};
+vrahoptionjwt.jwtFromRequest = vrajjwtextract.fromAuthHeaderWithScheme('jwt');
 
-let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-  console.log('payload received', jwt_payload);
+
+
+
+
+
+
+vrahoptionjwt.secretOrKey = process.env.JWT_SECRET;
+
+let vplan = new vrajplan(vrahoptionjwt, function (jwt_payload, next) {
+
+
+    console.log('payload received', jwt_payload);
 
   if (jwt_payload) {
-    // The following will ensure that all routes using
-    // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values
-    // that matches the request payload data
+ 
     next(null, {
-      _id: jwt_payload._id,
+  
+  
+        _id: jwt_payload._id,
       userName: jwt_payload.userName
 
     });
@@ -35,14 +43,18 @@ let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
     next(null, false);
   }
 });
-// tell passport to use our "strategy"
-passport.use(strategy);
 
-// add passport as application-level middleware
+passport.use(vplan);
+
+
+
+
 app.use(passport.initialize());
+
 
 app.use(express.json());
 app.use(cors());
+
 
 app.post("/api/user/register", (req, res) => {
 
@@ -57,12 +69,12 @@ app.post("/api/user/register", (req, res) => {
 app.post("/api/user/login",  (req, res) => {
     userService.checkUser(req.body)
     .then((user) => {
-        let payload = {
+        let vppayload = {
             _id: user._id,
             userName: user.userName
           };
           
-          let token = jwt.sign(payload, jwtOptions.secretOrKey);   
+          let token = jwt.sign(vppayload, vrahoptionjwt.secretOrKey);   
         res.json({ message: "login successful", token : token});
     }).catch(msg => {
         res.status(422).json({ message: msg });
